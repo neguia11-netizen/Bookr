e5fa40604ef1440d21534d5c37ab9e0eece9c463
neguia11-netizen <neguia11@gmail.com>
2026-04-14 23:05:07 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Success from './Success.jsx'
+
+const path = window.location.pathname;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {path === '/success' ? <Success /> : <App />}
   </React.StrictMode>,
 )
