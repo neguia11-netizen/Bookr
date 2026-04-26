1d45c33fb2c6df756d815284868d5d82ad4087d6
neguia11-netizen <neguia11@gmail.com>
2026-04-26 01:28:53 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import Success from './Success.jsx'
 import Portfolio from './Portfolio.jsx'
 import Admin from './Admin.jsx'
+import GiftCard from './GiftCard.jsx'
 
 const path = window.location.pathname;
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {path === '/success' ? <Success />
     : path === '/portfolio' ? <Portfolio />
     : path === '/admin' ? <Admin />
+    : path === '/giftcard' ? <GiftCard />
     : <App />}
   </React.StrictMode>,
 )
